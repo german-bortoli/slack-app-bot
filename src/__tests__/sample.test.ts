817f3968dc7d9e5faa82f42c025b86b25f490d60
German Bortoli <germanazo@gmail.com>
2022-05-11 15:33:40 -0300

    Installed slack bolt, did some research how to read messages and publish home page

---
+++ src/__tests__/sample.test.ts
@@ -0,0 +1,5 @@
+describe('Should test hello world', () => {
+    it('should return hello world', () => {
+        expect('Hello World').toBe('Hello World');
+    });
+});
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,59 @@
 import 'dotenv/config'; // To use our .env
-import { sayHello } from './hello';
+import { App, LogLevel } from '@slack/bolt';
 
-async function main() {
-  sayHello();
-}
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+  logLevel: LogLevel.DEBUG,
+});
 
-main();
+// All the room in the world for your code
+(async () => {
+
+
+  app.event('message', async ({ event, client, logger }) => {
+      console.log('🫣', event)
+  });
+
+  app.event('app_home_opened', async ({ event, client, logger }) => {
+  
+    try {
+      // Call views.publish with the built-in client
+      const result = await client.views.publish({
+        // Use the user ID associated with the event
+        user_id: event.user,
+        view: {
+          // Home tabs must be enabled in your app configuration page under "App Home"
+          "type": "home",
+          "blocks": [
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "*Welcome home, <@" + event.user + "> :house:*"
+              }
+            },
+            {
+              "type": "section",
+              "text": {
+                "type": "mrkdwn",
+                "text": "Learn how home tabs can be more useful and interactive <https://api.slack.com/surfaces/tabs/using|*in the documentation*>."
+              }
+            }
+          ]
+        }
+      });
+  
+      logger.info(result);
+    }
+    catch (error) {
+      logger.error(error);
+    }
+  });
+
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+  console.log('⚡️ Bolt app is running!');
+})();
+
